a582ac4e06a684d6655ef14744f5a1e4303a8590
nick <nikoladarkangel@gmail.com>
2023-07-16 20:27:22 +0300

    done 1, 2 task

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-import faker
-
-
-
-def print_name():
-    print(faker.Faker().name())
+from app.services import read_file
+from app.services import generate_users
 
 
 if __name__ == '__main__':
-    print_name()
-    print('hello')
+    print("------1------")
+    print(read_file.get_file_content('../files_input/data_input.txt'))
+    print("------2------")
+    users = generate_users.generate(num_users=100)
+    for user in users:
+        print(user)
+    print("------3------")

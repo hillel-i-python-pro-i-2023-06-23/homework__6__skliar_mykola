fe5dfe2dcae0cf0ba31e7cc3edbaaa6c91ead13b
nick <nikoladarkangel@gmail.com>
2023-07-17 01:04:59 +0300

    finish coding

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 from app.services import read_file
 from app.services import generate_users
 from app.services import who_is_here
+from app.services import midle
+import os
 
 
-if __name__ == "__main__":
+def run():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
     print("------1------")
-    print(read_file.get_file_content("../files_input/data_input.txt"))
+    link_1 = os.path.join(current_dir, "../files_input/data_input.txt")
+    print(read_file.get_file_content(link_1))
     print("------2------")
     users = generate_users.generate(num_users=10)
     for user in users:
         print(user)
     print("------3------")
-    link = "http://api.open-notify.org/astros.json"
-    print(who_is_here.parsing(link))
+    link_3 = "http://api.open-notify.org/astros.json"
+    print(who_is_here.parsing(link_3))
     print("------4------")
+    link_4 = os.path.join(current_dir, "../files_input/people_data(extended).csv")
+    print(f"height {midle.midle_w(link_4)[0]} weight {midle.midle_w(link_4)[1]}")
+
+
+if __name__ == "__main__":
+    run()
